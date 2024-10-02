7776b6ad939f8333a7c6f533caa8453979060842
PLUXINDUI <alexsun1503@gmail.com>
2024-10-02 18:48:25 +0300

    Start

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template> 
+    <Header />
+    <nuxt :key="$route.fullPath" />
+</template>
+
+<script>
+import { Header } from 'components/header/header';
+
+export default{
+    comments: {
+       Header 
+    }
+}
+</script>
